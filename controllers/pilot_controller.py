e5eb250bb3351eebcb4331f8189c63cdb205fd0f
Khun <khunly@bstorm.be>
2026-07-13 13:43:15 +0200

    feat: Update pilot controller to include cache invalidation and adjust cache expiration

--- controllers/pilot_controller.py
+++ controllers/pilot_controller.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import APIRouter, Body, Depends, Query
+from fastapi_cache import FastAPICache
 
 from dto import PilotAddDto, PilotDto
 from services import PilotService
@@ -9,16 +10,18 @@ from fastapi_cache.decorator import cache
 pilot_router = APIRouter(prefix='/api', tags=['Pilot'])
 
 @pilot_router.post('/pilot', status_code=201)
-def post(
+async def post(
         dto: PilotAddDto = Body(),
         pilot_service: PilotService = Depends(PilotService)
     ):
-    # TODO INVALIDATE CACHE HERE
     pilot = pilot_service.add_pilot(dto)
+    # EXERCICE: 
+    # TODO INVALIDATE CACHE HERE
+    await FastAPICache.clear(namespace='pilot')
     return pilot.id
 
 @pilot_router.get('/pilot')
-@cache(expire=60)
+@cache(expire=300, namespace='pilot')
 def get_pilots(
         sort_order: Optional[str] = Query(default=None),
         pilot_service: PilotService = Depends(PilotService)
